c09c1f13eae3320309702c62622fe8ff2a5aaa62
Yu Yu <vunoooo1111@gmail.com>
2024-09-29 22:24:07 +0700

    add future aggstrade realtime

--- app.py
+++ app.py
@@ -4,25 +4,32 @@ from flask_cors import CORS
 from connections.redis_conn import r
 
 app = Flask(__name__)
-CORS(app, origins=["http://127.0.0.1:5500"])
+CORS(app, origins=["http://127.0.0.1:5500", "http://localhost:8989"])
 # Redis connection
 
-def event_stream(symbol):
+def event_stream(channel):
     pubsub = r.pubsub()
-    channel = f'order_book_channel_{symbol}'  # Subscribe to a symbol-specific channel
     pubsub.subscribe(channel)
 
-    # Stream the messages to the client
     for message in pubsub.listen():
         if message['type'] == 'message':
             print(f"Received message: {message['data'].decode('utf-8')}")
             yield f"data: {message['data'].decode('utf-8')}\n\n"
 
-# Stream Order Book for a specific symbol
+
 @app.route('/stream/order_book')
 def stream_order_book():
     symbol = request.args.get('symbol', 'btcusd_perp')
-    return Response(event_stream(symbol), content_type='text/event-stream')
+    channel = f'order_book_channel_{symbol}'
+
+    return Response(event_stream(channel), content_type='text/event-stream')
+
+
+@app.route('/stream/aggs_trade')
+def stream_aggs_trade():
+    symbol = request.args.get('symbol', 'btcusd_perp')
+    channel = f'aggs_trade_channel_{symbol}'
+    return Response(event_stream(channel), content_type='text/event-stream')
 
 if __name__ == "__main__":
     app.run(debug=True)
